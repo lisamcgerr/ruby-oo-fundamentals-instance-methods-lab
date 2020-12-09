41242d1a64a8f83d279635bd8831e5909971efac
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2020-12-08 23:37:28 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+
+    def bark
+        @bark = puts "Woof"
+    end
+    def sit
+        @sit = puts "The Dog is sitting"
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+    def talk
+        @talk = puts "Hello World!"
+    end
+
+    def walk
+        @walk = puts "The Person is walking"
+    end
+end
